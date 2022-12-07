ea1b158944a1382f087dfaee29412784e9837179
Ayush <ayushchopra0@gmail.com>
2022-12-08 00:14:53 +0530

    NumberOfProvinces

--- NumberOfProvinces.cpp
+++ NumberOfProvinces.cpp
@@ -1,59 +1,79 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class Solution {
-  private: 
-    // dfs traversal function 
-    void dfs(int node, vector<int> adjLs[], int vis[]) {
-        // mark the more as visited
-        vis[node] = 1; 
-        for(auto it: adjLs[node]) {
-            if(!vis[it]) {
-                dfs(it, adjLs, vis); 
+class Solution
+{
+public:
+    void bfs(vector<vector<char>> &grid, vector<vector<int>> &isVisited, int i, int j)
+    {
+        isVisited[i][j] = 1;
+        queue<pair<int, int>> q;
+        q.push({i, j});
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        while (!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            // to add the adjacent neighbours of the given q.front()
+            // row const and col-1 col+1
+            if (col - 1 >= 0 && !isVisited[row][col - 1] && grid[row][col - 1] == '1')
+            {
+                isVisited[row][col - 1] = 1;
+                q.push({row, col - 1});
+            }
+
+            if (col + 1 < m && !isVisited[row][col + 1] && grid[row][col + 1] == '1')
+            {
+                isVisited[row][col + 1] = 1;
+                q.push({row, col + 1});
+            }
+
+            // col const and row-1 row+1
+
+            if (row - 1 >= 0 && !isVisited[row - 1][col] && grid[row - 1][col] == '1')
+            {
+                isVisited[row - 1][col] = 1;
+                q.push({row - 1, col});
+            }
+
+            if (row + 1 < n && !isVisited[row + 1][col] && grid[row + 1][col] == '1')
+            {
+                isVisited[row + 1][col] = 1;
+                q.push({row + 1, col});
             }
         }
     }
-  public:
-    int numProvinces(vector<vector<int>> adj, int V) {
-        vector<int> adjLs[V]; 
-        
-        // to change adjacency matrix to list 
-        for(int i = 0;i<V;i++) {
-            for(int j = 0;j<V;j++) {
-                // self nodes are not considered
-                if(adj[i][j] == 1 && i != j) {
-                    adjLs[i].push_back(j); 
-                    adjLs[j].push_back(i); 
+
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt = 0;
+        vector<vector<int>> isVisited(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (!isVisited[i][j] && grid[i][j] == '1')
+                {
+                    cnt++;
+                    bfs(grid, isVisited, i, j);
                 }
             }
         }
-        int vis[V] = {0}; 
-        int cnt = 0; 
-        for(int i = 0;i<V;i++) {
-            // if the node is not visited
-            if(!vis[i]) {
-                // counter to count the number of provinces 
-                cnt++;
-               dfs(i, adjLs, vis); 
-            }
-        }
-        return cnt; 
-        
+
+        return cnt;
     }
 };
 
-int main() {
-    
-    vector<vector<int>> adj
-    {
-        {1, 0, 1},
-        {0, 1, 0},
-        {1, 0, 1}
-    };
+int main()
+{
 
-        
-    Solution ob;
-    cout << ob.numProvinces(adj,3) << endl;
-        
     return 0;
 }
